7c61055bba08bdcd7364886a498cfed45676e98a
sosoboso71 <chetansorin@gmail.com>
2026-04-07 14:22:45 +0300

    Refactor game.js for improved readability

--- game.js
+++ game.js
@@ -168,7 +168,7 @@ function gameLoop() {
     player.style.top = playerY + "px";
 
     hudPlatform.style.left = (playerX - 20) + "px";
-    hudPlatform.style.top = "460px";
+    hudPlatform.style.top = (playerY + PLAYER_HEIGHT + 10) + "px";
 
     updatePlatforms();
     handleCollisions();
